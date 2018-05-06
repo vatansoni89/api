6a620b0aba4a5c3ce047e436ebd0228d24abe42f
vatansoni <vatansoni89@gmail.com>
2018-05-06 12:32:20 +0530

    API creation using nodeJS
    npm init
    npm install express
    
    get put post delete

---
+++ app.js
@@ -0,0 +1,40 @@
+var express = require("express");
+var app = express();
+
+var route = express.Router();
+
+// parser start
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+//end
+
+route.get('/test',function(req,res){
+    res.json({message:"Hello World"})
+})
+
+
+
+route.post('/test', function(req,res){
+    console.log('The body record is', req.body);
+    res.json({message: "Record saved successfully"});
+})
+
+route.put('/test/:id', function(req,res){
+    console.log("The id is " ,req.param.id);
+    console.log("The body is", req.body);
+    res.json({message:'Record updated.....'});
+})
+
+route.delete('/test/:id', function(req,res){
+    console.log("The id is " ,req.param.id);
+    console.log("The body is", req.body);
+    res.json({message:'Record deleted.....'});
+})
+
+app.use('/api', route)
+
+app.listen(3003, function(req,res){
+
+    console.log("Server starts")
+})
