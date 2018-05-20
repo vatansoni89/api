e0556a77162b23f93300493dc62e6eb5be3e5f45
vatansoni <vatansoni89@gmail.com>
2018-05-20 14:01:34 +0530

    File Upload in API

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+var multer = require("multer");
 var express = require("express");
 var app = express();
 
@@ -5,7 +7,7 @@ var route = express.Router();
 
 // parser start
 var bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({extended: true})) // By default encoding is UTF-8
 app.use(bodyParser.json())
 //end
 
@@ -30,6 +32,25 @@ route.delete('/test/:id', function(req,res){
     console.log("The id is " ,req.param.id);
     console.log("The body is", req.body);
     res.json({message:'Record deleted.....'});
+})
+
+var storage = multer.diskStorage({
+    destination: function(req,file,cb){
+        cb(null,'uploads/pics')
+    },
+    filename:function(req,file,cb)
+    {
+        console.log("File is ",file)
+      //  console.log(file,req)
+        cb(null,file.fieldname+'-'+Date.now())
+    }
+})
+
+var upload = multer({storage:storage});
+
+route.post('/test1', upload.single('pic'), function(req,res){
+console.log('The body record is',req.body);
+res.json({message:'File uploaded successfully'})
 })
 
 app.use('/api', route)
